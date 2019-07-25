f1bc79ea2af916bed1fe3bff354171fc77c7728c
Willem Serruys <serruys.willem@hotmail.com>
2019-07-26 01:10:33 +0200

    intial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Test.Models;
+
+namespace Test.Controllers
+{
+    public class Vertegenwoordiger
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+    public class HomeController : Controller
+    {
+        private List<Vertegenwoordiger> _listVertegenwoordigers;
+
+        public IActionResult Index()
+        {
+
+            _listVertegenwoordigers = new List<Vertegenwoordiger>
+            {
+                new Vertegenwoordiger{ID = 1,Name = "Jos"},
+                new Vertegenwoordiger{ID = 2,Name = "Pol"},
+                new Vertegenwoordiger{ID = 3,Name = "Tom"}
+            };
+            return View(_listVertegenwoordigers);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/VertegenwoordigerViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Test.Models;
+
+namespace Test.Controllers
+{
+    public class VertegenwoordigerViewComponent: ViewComponent
+    {
+        public  IViewComponentResult Invoke()
+        {
+            return  View("VertegenwoordigerView",new VertegenwoordigerModel());
+        }
+
+        // POST: Devices/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public  IViewComponentResult Create([Bind("Name")] VertegenwoordigerModel vwm)
+        {
+            if (ModelState.IsValid)
+            {
+
+                return View("VertegenwoordigerView", new VertegenwoordigerModel());
+            }
+            return View("VertegenwoordigerView", vwm);
+        }
+    }
+}
